586535106a5594a65726ed2c74f065d241a6bfdc
Your Name <Your Name>
2023-07-28 06:30:10 -1100

    first commit

---
+++ routes/categorie.route.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const {verifyToken} =require("../middleware/verifyToken")
+const router = express.Router();
+const Categorie=require("../models/categorie")
+const {authorizeRoles} = require("../middleware/authorizeRoles")
+// afficher la liste des categories.
+router.get('/',verifyToken,authorizeRoles("user","admin","visiteur"),async (req, res)=> {
+try {
+const cat = await Categorie.find();
+
+res.status(200).json(cat);
+} catch (error) {
+res.status(404).json({ message: error.message });
+}
+
+});
+// créer un nouvelle catégorie
+router.post('/', async (req, res) => {
+const { nomcategorie, imagecategorie} = req.body;
+const newCategorie = new Categorie({nomcategorie:nomcategorie,
+imagecategorie:imagecategorie})
+
+try {
+await newCategorie.save();
+
+res.status(200).json(newCategorie );
+} catch (error) {
+res.status(404).json({ message: error.message });
+}
+
+});
+// chercher une catégorie
+router.get('/:categorieId',async(req, res)=>{
+try {
+const cat = await Categorie.findById(req.params.categorieId);
+res.status(200).json(cat);
+} catch (error) {
+res.status(404).json({ message: error.message });
+}
+});
+// modifier une catégorie
+router.put('/:categorieId', async (req, res)=> {
+const { nomcategorie, imagecategorie} = req.body;
+const id = req.params.categorieId;
+try {
+const cat1 = {
+nomcategorie:nomcategorie,imagecategorie:imagecategorie, _id:id };
+await Categorie.findByIdAndUpdate(id, cat1);
+res.json(cat1);
+} catch (error) {
+res.status(404).json({ message: error.message });
+}
+});
+// Supprimer une catégorie
+router.delete('/:categorieId', async (req, res)=> {
+const id = req.params.categorieId;
+await Categorie.findByIdAndDelete(id);
+
+res.json({ message: "categorie deleted successfully." });
+});
+module.exports = router;
---
+++ routes/user.route.js
@@ -0,0 +1,132 @@
+const express = require('express');
+const router = express.Router();
+const User=require("../models/user");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const nodemailer=require('nodemailer');
+var transporter =nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+    user:'esps421@gmail.com',
+    pass:'larjhlhczipojrlg'
+    },
+    tls:{
+    rejectUnauthorized:false
+    }
+    })
+// créer un nouvel utilisateur
+router.post('/register', async (req, res) => {
+try {
+let { email, password, firstname, lastname } = req.body
+const user = await User.findOne({ email })
+if (user) return res.status(404).send({ success: false, message:
+
+"User already exists" })
+
+const newUser = new User({ email, password, firstname, lastname })
+const createdUser = await newUser.save()
+// Envoyer l'e-mail de confirmation de l'inscription
+var mailOption ={
+    from: '"verify your email " <esps421@gmail.com>',
+    to: newUser.email,
+    subject: 'vérification your email ',
+    html:`<h2>${newUser.firstname}! thank you for registreting on our website</h2>
+    <h4>please verify your email to procced.. </h4>
+    <a
+    href="http://${req.headers.host}/api/users/status/edit?email=${newUser.email}">click
+    here</a>`
+    }
+    transporter.sendMail(mailOption,function(error,info){
+    if(error){
+    console.log(error)
+    }
+    else{
+    console.log('verification email sent to your gmail account ')
+    }
+    })
+return res.status(201).send({ success: true, message: "Account created successfully", user: createdUser })
+} catch (err) {
+console.log(err)
+res.status(404).send({ success: false, message: err })
+}
+});
+// afficher la liste des utilisateurs.
+router.get('/', async (req, res, )=> {
+    try {
+    const users = await User.find().select("-password");
+    res.status(200).json(users);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    
+    });
+    /**
+* as an admin i can disable or enable an account
+*/
+router.get('/status/edit/', async (req, res) => {
+    try {
+    let email = req.query.email
+    let user = await User.findOne({email})
+    user.isActive = !user.isActive
+    user.save()
+    res.status(200).send({ success: true, user })
+    } catch (err) {
+    return res.status(404).send({ success: false, message: err })
+    }
+    })
+    // se connecter
+router.post('/login', async (req, res) => {
+    try {
+    let { email, password } = req.body
+    
+    if (!email || !password) {
+    
+    
+    return res.status(404).send({ success: false, message: "Allfields are required" })
+    
+    }
+    
+    let user = await User.findOne({ email
+    }).select('+password').select('+isActive')
+    
+    if (!user) {
+    
+    return res.status(404).send({ success: false, message: "Accountdoesn't exists" })
+    
+    } else {
+    
+    let isCorrectPassword = await bcrypt.compare(password, user.password)
+    if (isCorrectPassword) {
+    
+    delete user._doc.password
+    if (!user.isActive) return res.status(200).send({ success:
+    
+    false, message: 'Your account is inactive, Please contact your administrator' })
+    
+    const token = jwt.sign ({ iduser:
+    
+    user._id,name:user.firstname, role: user.role }, process.env.SECRET, {
+    expiresIn: "1h", })
+    
+    return res.status(200).send({ success: true, user, token })
+    
+    } else {
+    
+    return res.status(404).send({ success: false, message:
+    
+    "Please verify your credentials" })
+    
+    
+    
+    }
+    
+    }
+    
+    } catch (err) {
+    return res.status(404).send({ success: false, message: err.message
+    
+    })
+    }
+    
+    });
+module.exports = router;
